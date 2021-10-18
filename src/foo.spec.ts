c747102bf7232237a101b1fef5e515d42715c145
Thibaud Desodt <thibaud.desodt@koahealth.com>
2021-10-18 14:50:07 +0200

    remove old foo.* files

--- src/foo.spec.ts
+++
@@ -1,9 +0,0 @@
-import { doIt } from './foo';
-
-describe('A Test', () => {
-  test('should return true', () => {
-    const actual = doIt();
-
-    expect(actual).toBe(true);
-  });
-});
--- src/foo.ts
+++
@@ -1,3 +0,0 @@
-export function doIt() {
-  return false;
-}
